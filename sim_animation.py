782f087862fc65ebf46b44f203d47ebbaa0ff0a8
helenajr <helenajrobinson@gmail.com>
2024-12-24 11:15:53 +0000

    animation up and running

--- sim_animation.py
+++ sim_animation.py
@@ -2,12 +2,13 @@ import simpy
 import random
 import pandas as pd
 from vidigi.utils import populate_store
+from vidigi.animation import animate_activity_log
 
 
 class g:
-    patient_inter = 5
+    patient_inter = 1
     mean_n_consult_time = 6
-    number_of_nurses = 2
+    n_cubicles = 2
     sim_duration = 120
     number_of_runs = 5
 
@@ -25,7 +26,7 @@ class Model:
     
     def init_resources(self):
         self.nurse = simpy.Store(self.env)
-        populate_store(num_resources=g.number_of_nurses,
+        populate_store(num_resources=g.n_cubicles,
                        simpy_store=self.nurse,
                        sim_env=self.env)
 
@@ -121,3 +122,47 @@ my_trial = Trial()
 my_trial.run_trial()
 
 my_trial.all_event_logs.tail(100)
+
+event_position_df = pd.DataFrame([
+                    {'event': 'arrival',
+                     'x':  50, 'y': 300,
+                     'label': "Arrival" },
+
+                    # Triage - minor and trauma
+                    {'event': 'treatment_wait_begins',
+                     'x':  205, 'y': 170,
+                     'label': "Waiting for Treatment"},
+
+                    {'event': 'treatment_begins',
+                     'x':  205, 'y': 110,
+                     'resource':'n_cubicles',
+                     'label': "Being Treated"},
+
+                    {'event': 'depart',
+                     'x':  270, 'y': 70,
+                     'label': "Exit"}
+
+                ])
+
+animate_activity_log(
+        event_log=my_trial.all_event_logs[my_trial.all_event_logs['run']==1],
+        event_position_df= event_position_df,
+        scenario=g(),
+        debug_mode=True,
+        every_x_time_units=1,
+        include_play_button=True,
+        icon_and_text_size=20,
+        gap_between_entities=6,
+        gap_between_rows=15,
+        plotly_height=700,
+        frame_duration=200,
+        plotly_width=1200,
+        override_x_max=300,
+        override_y_max=500,
+        limit_duration=g.sim_duration,
+        wrap_queues_at=25,
+        step_snapshot_max=125,
+        time_display_units="dhm",
+        display_stage_labels=False,
+        add_background_image="Simplest Model Background Image - Horizontal Layout.drawio.png",
+    )
